6ac3fd6b7c0a27790b6d07284c0b15ca959c014e
unknown <matheustsr@outlook.com>
2021-06-19 11:05:06 -0300

    saving session in LocalStorage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Todo } from './../models/todo.model';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root', // tag HTML
@@ -9,25 +10,52 @@ import { Component } from '@angular/core';
 export class AppComponent { //make public
   public todos: Todo[] = [];
   public title: string = "Meus projetos";
+  public form: FormGroup;
 
-  constructor() {
-    this.todos.push(new Todo(1 ,'Passear com Marley', false));
-    this.todos.push(new Todo(2 ,'Ir no mercado', false));
-    this.todos.push(new Todo(3 ,'Cortar o cabelo', true));
+  constructor(private fb: FormBuilder) {
+
+    this.form = this.fb.group({
+      title: ['', Validators.compose([
+        Validators.minLength(3),
+        Validators.maxLength(60),
+        Validators.required
+      ])]
+    });
   }
 
-  remove(todo: Todo){
-    const index = this.todos.indexOf(todo); // get task index
-    if(index !== -1){
-      this.todos.splice(index, 1); // delete one index of list
+  addTask(){
+    const title = this.form.controls['title'].value;
+    const id = this.todos.length + 1;
+    this.todos.push(new Todo(id, title, false));
+    this.saveToLocalStorage();
+    this.clear();
+  }
+
+  clear(){
+    this.form.reset();
+  }
+  removeTask(todo: Todo){
+      const index = this.todos.indexOf(todo); // get task index
+      if(index !== -1){
+        this.todos.splice(index, 1); // delete one index of list
+      }
     }
-  }
 
   markAsDone(todo: Todo){
-    todo.done = true;
+      todo.done = true;
   }
 
   markAsUndone(todo: Todo){
-    todo.done = false;
+      todo.done = false;
+  }
+
+  saveToLocalStorage(){
+    const data = JSON.stringify(this.todos);
+    localStorage.setItem('todos', data);
+  }
+
+  loadData(){
+    const data = localStorage.getItem('todos');
   }
 }
+
